0929431776180943dde65e1ddba93a8c088c030d
Udhayakumar.M <147937372+Udhaya123S@users.noreply.github.com>
2024-09-03 19:05:10 +0530

    Add files via upload

---
+++ controllers/index.js
@@ -0,0 +1,4 @@
+module.exports.countryController = require('./country.controller');
+module.exports.cityController = require('./city.controller');
+module.exports.universityController = require('./university.controller');
+module.exports.courseController = require('./course.controller');
