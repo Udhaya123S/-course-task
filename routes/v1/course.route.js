7b5fcbdd37aa629a4eb0a019371179894e6c4b99
Udhayakumar.M <147937372+Udhaya123S@users.noreply.github.com>
2024-09-03 19:06:26 +0530

    Add files via upload

---
+++ routes/v1/course.route.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const courseController = require('../../controllers/course.controller');
+
+router.post('/',  courseController.createCourse);
+
+router.post('/upload',  courseController.uploadCsv);
+
+router.get('/', courseController.getCourses);
+
+router.get('/:id', courseController.getCourseById); 
+
+router.put('/:id', courseController.updateCourseById);
+
+router.delete('/:id', courseController.deleteCourseById);
+
+module.exports = router;
---
+++ routes/v1/university.route.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const universityController = require('../../controllers/university.controller');
+
+const router = express.Router();
+
+
+router.post('/', universityController.createUniversity);
+
+router.get('/', universityController.getUniversities);
+
+router.get('/:id', universityController.getUniversityById);
+
+router.put('/:id', universityController.updateUniversity);
+
+router.delete('/:id', universityController.deleteUniversity);
+
+module.exports = router;
